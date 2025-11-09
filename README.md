dc1550e011c4de606d9760e03bf827c9da328e74
JasonGrace2282 <aarush.deshpande@gmail.com>
2025-11-09 10:53:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Sysadmin Aliases
+These are a collection of aliases used frequently by TJ CSL Sysadmins.
+
+## Installation
+You must have the following installed:
+
+- `gpg`
+- `sshpass`
+- [`fzf`](https://github.com/junegunn/fzf)
+- [`fd`](https://github.com/sharkdp/fd)
+
+Additionally, the following environment variables must be set:
+- `KEYBASE_PASSCARD_DIR` must be set to the location of the git clone of `keybase-passcard`.
+- `ANSIBLE_DIR` must be set to the location of the git clone of `ansible`
+
+To use some aliases, you may also need to have a bash function called `copy`
+to copy something to clipboard from stdin for your system (e.g. `wl-copy`
+for Wayland, `xclip -sel clip` for xorg, etc).
+
+After that, simply clone this repository and source `main.sh`
---
+++ main.sh
@@ -0,0 +1,162 @@
+if [ -z "$KEYBASE_PASSCARD_DIR" ]; then
+  echo "Error: KEYBASE_PASSCARD_DIR is not set." >&2
+  return 1
+fi
+
+if [ -z "$CSL_ANSIBLE_DIR" ]; then
+  echo "Error: CSL_ANSIBLE_DIR is not set." >&2
+  return 1
+fi
+
+PASSCARD_DIR="${KEYBASE_PASSCARD_DIR}/passwords"
+TEMP_RUNNER_FILE="${TEMP_RUNNER_FILE:-$HOME/.ansible-playbook-runner.sh}"
+
+raw-passcard() {
+  if [ $# -ne 1 ]; then
+    echo "Usage: raw-passcard <passcard-name>" >&2
+    return 1
+  fi
+  output="${1%.txt.gpg}"
+  gpg -d "$PASSCARD_DIR/$output.txt.gpg" 2>/dev/null
+}
+
+pp() {
+  if [[ "$1" =~ ^(-h|--help)?$ ]]; then
+    echo "Usage: pp <passcard-name>"
+    echo "Copy a passcard to clipboard"
+    return 0
+  fi
+  passcard="$(raw-passcard "$1")"
+  # shellcheck disable=SC2181
+  if [ $? -ne 0 ]; then
+    echo "Error: Unable to decrypt passcard '$1'" >&2
+    return 1
+  fi
+  copy <<< "$passcard"
+}
+
+p() {
+  if [[ "$1" =~ ^(-h|--help)$ ]]; then
+    echo "Fuzzy search for a passcard and copy it"
+    return 0
+  fi
+  cd "$PASSCARD_DIR" || return 1
+  passcard="$(fd | fzf)"
+  [ -n "$passcard" ] && pp "$passcard"
+}
+
+j() {
+  if [[ -z "$1" ]]; then
+    cat <<EOF
+Usage: j <hostname> <ssh args>
+Copies the passcard for the given hostname and SSHes into it as root.
+EOF
+    return 0
+  fi
+  base="${1%.csl.tjhsst.edu}"
+  base="${base%.tjhsst.edu}"
+  if [[ "$base" == "borg"* || "$base" == "hpc"* ]]; then
+    password="$(raw-passcard cluster)"
+  else
+    password="$(raw-passcard "$base")"
+  fi
+  # shellcheck disable=SC2181
+  if [[ "$?" -ne 0 ]]; then
+    echo "Error: Unable to retrieve passcard for '$base'" >&2
+    return 1
+  fi
+  if [[ "$1" != *".tjhsst.edu" ]]; then
+    if [[ "$base" =~ ^(www|ipa) ]]; then
+      set -- "$1.tjhsst.edu"
+    else
+      set -- "$1.csl.tjhsst.edu"
+    fi
+  fi
+  url="$1"
+  shift
+  sshpass -p "$password" ssh "root@$url" "$@"
+}
+
+
+tjans() {
+    NUM_FORKS="100"
+    CONNECT_USER="root"
+    PLAY="$1"
+    SSH_PASS_NAME=""
+    VAULT_PASS_NAME="ansible"
+
+    if [[ "$PLAY" == "" ]]
+    then
+        echo "usage: tjans (playbook) [options]..."
+        return
+    fi
+
+    if [[ "$PLAY" != "-h" ]] && [[ "$PLAY" != "--help" ]]
+    then
+        shift
+    fi
+
+    other_args=()
+
+    while [[ $# -gt 0 ]]
+    do
+        case $1 in
+            -h | --help)
+                echo "usage: tjans (playbook) [options]..."
+                echo Run a tjCSL ansible play intelligently
+                echo
+                echo "  -p, --pass PASS                 Specify the name of the passcard file to use when connecting"
+                echo "  -v, --vault, --vault-pass PASS  Specify the name of the vault passcard file (excluding \"_vault\") to use"
+                echo "  -u, --user USERNAME             Specify the username to connect with"
+                echo "  -f, --forks N                   Set the number of concurrent processes to use at once"
+                echo
+                echo "  ...any other valid ansible-playbook options are also permitted and will be passed to ansible"
+                return
+                ;;
+            -p | --pass)
+                SSH_PASS_NAME="$2"
+                shift
+                shift
+                ;;
+            -v | --vault | --vault-pass)
+                VAULT_PASS_NAME="$2"
+                shift
+                shift
+                ;;
+            -u | --user)
+                CONNECT_USER="$2"
+                shift
+                shift
+                ;;
+            -f | --forks)
+                NUM_FORKS="$2"
+                shift
+                shift
+                ;;
+            --* | -*)
+                other_args+=("$1")
+                shift
+                ;;
+            *)
+                other_args+=("$1")
+                shift
+                ;;
+        esac
+    done
+
+    set -- "${other_args[@]}"
+
+    pp "$SSH_PASS_NAME" || true
+    sshpass="$(raw-passcard "$SSH_PASS_NAME")"
+    echo "$sshpass"
+
+    vaultpass=$(raw-passcard "$VAULT_PASS_NAME"_vault)
+    echo "#!/bin/bash" > "$TEMP_RUNNER_FILE"
+    echo "echo $vaultpass" >> "$TEMP_RUNNER_FILE"
+    chmod +x "$TEMP_RUNNER_FILE"
+
+    echo "RUNNING COMMAND:"
+    echo "    " ansible-playbook "$CSL_ANSIBLE_DIR"/"$PLAY".yml -i "$CSL_ANSIBLE_DIR"/hosts -f "$NUM_FORKS" -u "$CONNECT_USER" "$@"
+    git -C "$CSL_ANSIBLE_DIR" pull
+    ansible-playbook "$CSL_ANSIBLE_DIR"/"$PLAY".yml -i "$CSL_ANSIBLE_DIR"/hosts --ask-pass --vault-password-file "$TEMP_RUNNER_FILE" -f "$NUM_FORKS" -u "$CONNECT_USER" "$@"
+}
